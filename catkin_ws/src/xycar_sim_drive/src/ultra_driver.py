689e526b554c4501ce0f27ad0bb2cd51715c1545
Hongjae Shin <shjae1127@naver.com>
2021-05-03 02:06:43 +0900

    Prototype

--- catkin_ws/src/xycar_sim_drive/src/ultra_driver.py
+++ catkin_ws/src/xycar_sim_drive/src/ultra_driver.py
@@ -2,11 +2,14 @@
 
 import rospy, math
 from std_msgs.msg import Int32MultiArray
-
+ 
+    
 def callback(msg):
-    print(msg.data)
+   print(msg.data)
+   xycar_data.data = msg.data
 
 rospy.init_node('guide')
+xycar_data = Int32MultiArray()
 motor_pub = rospy.Publisher('xycar_motor_msg', Int32MultiArray, queue_size=1)
 ultra_sub = rospy.Subscriber('ultrasonic', Int32MultiArray, callback)
 
@@ -14,5 +17,16 @@ xycar_msg = Int32MultiArray()
 
 while not rospy.is_shutdown():
     angle = 0
-    xycar_msg.data = [angle, 10]
+    if xycar_data.data:
+        if xycar_data.data[1] < 300:
+            if xycar_data.data[0]>xycar_data.data[2]:
+                angle = angle - 50
+            else:
+                angle = angle + 50
+        else:
+            if xycar_data.data[7]>xycar_data.data[6]:
+                angle = angle - 50
+            else :
+                angle = angle+50
+    xycar_msg.data = [angle, 50]
     motor_pub.publish(xycar_msg)
