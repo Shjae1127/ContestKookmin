fe3f1f6d7d4b4adf62d203fa2df1fea9ea00417f
Hongjae Shin <shjae1127@naver.com>
2021-05-15 20:20:28 +0900

    pure pursuit
    좀 더 제대로 된 알고리즘을 이용하고자 pure pursuit 알고리즘을 가져옴
    
    <문제점>
    좌/우회전 시 충분한 조향각이 안나옴
    
    <해결방안>
    회전시에만 강제로 조향/새로운 알고리즘 도입

--- catkin_ws/src/xycar_sim_drive/src/ultra_driver.py
+++ catkin_ws/src/xycar_sim_drive/src/ultra_driver.py
@@ -1,46 +1,36 @@
 #!/usr/bin/env python
 
-import rospy, math
+import rospy
+import math
 from std_msgs.msg import Int32MultiArray
+
 
 def callback(msg):
     print(msg.data)
-    xycar_data.data = msg.data
+    distance.data = msg.data
+
 
 rospy.init_node('guide')
-xycar_data = Int32MultiArray()
+distance = Int32MultiArray()
 motor_pub = rospy.Publisher('xycar_motor_msg', Int32MultiArray, queue_size=1)
 ultra_sub = rospy.Subscriber('ultrasonic', Int32MultiArray, callback)
+
 xycar_msg = Int32MultiArray()
-
-
-stackTurn = 0
-stackStraightL = 0
-stackStraightR = 0
-angleAccel = 1
+angle = 0
 while not rospy.is_shutdown():
-    angle = 0  
-    if xycar_data.data:
-        if xycar_data.data[1] < 300:
-            stackStraightL = 0
-            stackStraightR = 0
-            stackTurn+=1
-            if xycar_data.data[0]>xycar_data.data[2]:
-                angle = angle - angleAccel * stackTurn^2
-            else:
-                angle = angle + angleAccel * stackTurn^2
-        else:
-            stackTurn = 0
-            if xycar_data.data[7]>xycar_data.data[6] + 10:
-                angle = angle - angleAccel * stackStraightL^2
-                stackStraightL+=1
-                stackStraightR = 0
-                
-            elif xycar_data.data[7]<xycar_data.data[6] - 10:
-                angle = angle + angleAccel * stackStraightR^2
-                stackStraightR+=1
-                stackStraightL = 0
-            else:
-                angle = 0
-    xycar_msg.data = [angle, 50]
+    if (distance.data):
+        fl = distance.data[0]
+        fm = distance.data[1]
+        fr = distance.data[2]
+        l = distance.data[7]
+        r = distance.data[6]
+        theta = math.radians(36.7)
+        ld_x = (fr-fl)*math.cos(theta)/2
+        ld_y = (fr+fl)*math.sin(theta)/2
+        ld = math.sqrt(pow(ld_x, 2)+pow(ld_y, 2))
+        alpha = math.atan(ld_x/ld_y)
+        L = 450/3.33
+        angle = math.atan((2*L*math.sin(alpha))/ld)
+        angle = math.degrees(angle)
+    xycar_msg.data = [angle, 15]
     motor_pub.publish(xycar_msg)
