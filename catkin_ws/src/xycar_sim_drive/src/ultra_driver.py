608e27c26a35f1da6c70c4813bb58034ac52e0e0
Hongjae Shin <shjae1127@naver.com>
2021-05-27 03:24:05 +0900

    add comment

--- catkin_ws/src/xycar_sim_drive/src/ultra_driver.py
+++ catkin_ws/src/xycar_sim_drive/src/ultra_driver.py
@@ -7,7 +7,7 @@ from std_msgs.msg import Int32MultiArray
 
 def callback(msg):
     print(msg.data)
-    distance.data = msg.data
+    distance.data = msg.data                                #save the data via ultrasonic sensor
 
 
 rospy.init_node('guide')
@@ -19,18 +19,26 @@ xycar_msg = Int32MultiArray()
 angle = 0
 while not rospy.is_shutdown():
     if (distance.data):
+        #####Variable Setting #####
         fl = distance.data[0]
         fm = distance.data[1]
         fr = distance.data[2]
         l = distance.data[7]
         r = distance.data[6]
         
-        theta = math.radians(36.7)
+        theta = math.radians(36.7)                                      #angle between fl and l (calculated using the arctangent of initial value)
+        ###########################################################
+        # case1(Straight Drive): Pure Pursuit                                                                                                #
+        #                  Target point : center of fl and fr                                                                                    #
+        #                   Y Axis : fm                                                                                                                             #
+        # case2(Turn) : set steering angle = 50                                                                                             #
+        #                   steering angle is insufficient when turning left or right to use Pure Pursuit #
+        ###########################################################
         ld_x = (fr-fl)*math.cos(theta)/2
         ld_y = (fr+fl)*math.sin(theta)/2
         ld = math.sqrt(pow(ld_x, 2)+pow(ld_y, 2))
         alpha = math.atan(ld_x/ld_y)
-        L = 164*2/3.33
+        L = 164*2/3.33                                                              # vehicle length (calculated using the ratio of inscreen length and sensor data)
         angle = math.atan((2*L*math.sin(alpha))/ld)
         angle = math.degrees(angle)
         if fm < 250:
