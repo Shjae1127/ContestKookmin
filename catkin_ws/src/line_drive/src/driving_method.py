dfbd976663b6493ce4ba559a4d21da253edf9bbd
yewon <jujuome9917@naver.com>
2021-05-27 20:40:41 +0900

    디버그 위한? 문장 삭제

--- catkin_ws/src/line_drive/src/driving_method.py
+++ catkin_ws/src/line_drive/src/driving_method.py
@@ -13,8 +13,4 @@ def getSteerAng(pos):
     center = (lpos+rpos)/2
     dif = Width/2 - center
     degree = dif * k
-    if degree < 50 and degree>-50:
-        print(degree)
-    elif degree > 50 or degree < -50:
-        print("---------------------------")
     return degree
